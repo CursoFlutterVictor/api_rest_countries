25bddb42c8318ec933bcbf70dfe36679ae6b2024
CursoFlutterVictor <cursofluttervictor@gmail.com>
2022-10-17 14:04:30 +0200

    master

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:api_rest_countries/models/country_model.dart';
 import 'package:api_rest_countries/services/service_countries.dart';
 
 Future<void> main() async {
-  List<Country>? paises = await ServiceCountriesApi.getCountry("España");
+  List<Country>? paises = await ServiceCountriesApi.getCountry("Peru");
 
   if (paises != null) {
     for (var p in paises) {
