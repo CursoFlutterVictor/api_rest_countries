c0f858c1c04b35f9e2263730394a58ac39a0f80b
CursoFlutterVictor <cursofluttervictor@gmail.com>
2022-10-17 14:04:02 +0200

    init git

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:api_rest_countries/models/country_model.dart';
+import 'package:api_rest_countries/services/service_countries.dart';
+
+Future<void> main() async {
+  List<Country>? paises = await ServiceCountriesApi.getCountry("España");
+
+  if (paises != null) {
+    for (var p in paises) {
+      print(p.name);
+    }
+  }
+}
---
+++ lib/services/service_countries.dart
@@ -0,0 +1,74 @@
+//import 'dart:convert';
+//import 'dart:developer';
+
+import 'dart:convert';
+import 'package:api_rest_countries/models/country_model.dart';
+import 'package:http/http.dart' as http;
+
+class ServiceCountriesApi {
+  /*
+  static void getCountries() {
+    var url = Uri.parse("https://restcountries.com/v2/all");
+    print(url);
+  }*/
+
+  static Future<List<Country>?> getCountry(String country) async {
+    var url = Uri.parse("https://restcountries.com/v2/name/$country");
+
+    try {
+      // Await the http get response, then decode the json-formatted response.
+      var response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        var jsonResponse = jsonDecode(response.body);
+
+        print(jsonResponse);
+
+        /*
+       final myJson = jsonDecode(respuesta.body);
+      Country c = Country.fromJson(myJson.first);
+      */
+
+        final List<dynamic> bodyDecoded = jsonDecode(response.body);
+        final paises = bodyDecoded.map((p) => Country.fromJson(p));
+        final List<Country> listaPaises = paises.toList();
+
+        return listaPaises;
+      }
+    } catch (error) {
+      print(error);
+      return null;
+    }
+  }
+}
+
+
+
+/*
+class ServicesReqResApi {
+  void getReqRepService() {
+    Person persons = Person();
+    var url = Uri.parse("https://reqres.in/api/users?page=2");
+
+    print("URL: $url");
+
+    // Hacemos la llamada a la api
+    http.get(url).then((respuesta) {
+      print(respuesta.body);
+
+      // Convertimos el JSON en un objeto Person
+      persons = Person.fromJson(jsonDecode(respuesta.body));
+
+      // Recorro la lista de personas. Obliga a meter el nullsafety ?
+      persons.data?.forEach((element) {
+        print(element.email);
+      });
+      // Seria igual que un try cath
+    }).catchError((err) {
+      log("ERROR $err");
+    });
+
+    print("hola desde mi api");
+  }
+}
+*/
